defe6e0bbc6fbd50bc8ce3fc98a6c0e2fe497838
Emmanuel Higgins <emmanuel.higgins@blueshift.one>
2022-03-07 10:47:59 +1100

    Update LocalizationsChanged event name

--- projects/ngx-grid-core/src/lib/events/general/localizations-changed.event.ts
+++ projects/ngx-grid-core/src/lib/events/general/localizations-changed.event.ts
@@ -2,7 +2,7 @@ import { IGridDataSource } from '../../typings/interfaces'
 import { BaseGridEventAbstract } from '../base-grid-event.abstract'
 import { GridEventsService } from '../grid-events.service'
 
-export class LocalizationsChanged extends BaseGridEventAbstract<IGridDataSource> {
-  public readonly eventName = 'LocalizationsChanged'
+export class LocalizationsChangedEvent extends BaseGridEventAbstract<IGridDataSource> {
+  public readonly eventName = 'LocalizationsChangedEvent'
   constructor(eventService: GridEventsService) { super(eventService) }
 }
