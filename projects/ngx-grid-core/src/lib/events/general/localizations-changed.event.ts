7aed62837e997c55a8209478b688ca4c735c5b7c
Emmanuel Higgins <emmanuel.higgins@blueshift.one>
2022-03-07 10:49:12 +1100

    Fix incorrect event payload

--- projects/ngx-grid-core/src/lib/events/general/localizations-changed.event.ts
+++ projects/ngx-grid-core/src/lib/events/general/localizations-changed.event.ts
@@ -1,8 +1,7 @@
-import { IGridDataSource } from '../../typings/interfaces'
 import { BaseGridEventAbstract } from '../base-grid-event.abstract'
 import { GridEventsService } from '../grid-events.service'
 
-export class LocalizationsChangedEvent extends BaseGridEventAbstract<IGridDataSource> {
+export class LocalizationsChangedEvent extends BaseGridEventAbstract<void> {
   public readonly eventName = 'LocalizationsChangedEvent'
   constructor(eventService: GridEventsService) { super(eventService) }
 }
