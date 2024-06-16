627079185260d032ec2e94e3e419ebd90fdc957a
Blueshift Staff <hello@blueshift.one>
2024-06-17 09:54:42 +1000

    Synced from the Blueshift Repository

--- projects/ngx-grid-core/src/lib/utils/clipboard.class.ts
+++ projects/ngx-grid-core/src/lib/utils/clipboard.class.ts
@@ -13,7 +13,42 @@ export class Clipboard {
 
   public async write(): Promise<void> {
     try {
-      await navigator.clipboard.write([new ClipboardItem(this._clipboardFormats)])
+      var isNavigatorClipboardAllowed = false
+      if (navigator.clipboard && window.isSecureContext && document.hasFocus()) {
+        if (navigator.permissions) {
+          navigator.permissions.query({ name: 'clipboard-read' as PermissionName })
+            .then(permissionStatus => {
+              if (permissionStatus.state === 'granted') {
+                isNavigatorClipboardAllowed = true
+              }
+            })
+        }
+      }
+      if (isNavigatorClipboardAllowed) {
+        await navigator.clipboard.write([new ClipboardItem(this._clipboardFormats)])
+      } else {
+        const textarea = document.createElement('textarea');
+        const clipboardData = (await this._clipboardFormats['text/plain']);
+        const text = clipboardData instanceof Blob ? await clipboardData.text() : clipboardData.toString();
+        textarea.value = text
+    
+        // Move the textarea outside the viewport to make it invisible
+        textarea.style.position = 'absolute';
+        textarea.style.left = '-99999999px';
+    
+        document.body.prepend(textarea);
+    
+        // highlight the content of the textarea element
+        textarea.select();
+    
+        try {
+          document.execCommand('copy');
+        } catch (err) {
+          console.log(err);
+        } finally {
+          textarea.remove();
+        }
+      }
     } catch(e) {
       console.error(e)
     }
